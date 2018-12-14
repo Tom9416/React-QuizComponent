7707799b815671594f627d9f5618599f4abf3404
Tom Parkin <thomas.parkin@bglgroup.co.uk>
2018-12-14 15:56:02 +0000

    section 5 fixes

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component {
         const isQuizEnd = ((this.quiz_position - 1)) === quizData.quiz_questions.length;
         return ( 
             <div> 
-                {isQuizEnd ?<QuizEnd/> :
+                {isQuizEnd ? <QuizEnd /> :
                 <QuizQuestion quiz_question={quizData.quiz_questions
                 [this.state.quiz_position - 1]}/>}
                  </div>
