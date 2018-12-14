0e617a5fce69ba79cb2e8edfc779f9bdb95302f7
Tom Parkin <thomas.parkin@bglgroup.co.uk>
2018-12-14 16:01:54 +0000

    section 5 fixes

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js';
 import QuizEnd from './QuizEnd.js';
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
@@ -9,7 +10,7 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
     render() { 
-        const isQuizEnd = ((this.quiz_position - 1)) === quizData.quiz_questions.length;
+        const isQuizEnd = ((this.quiz_position - 1) === quizData.quiz_questions.length);
         return ( 
             <div> 
                 {isQuizEnd ? <QuizEnd /> :
